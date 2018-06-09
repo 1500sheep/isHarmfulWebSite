1af326b78f11253ce4ac1c61f92c8334aeb904b3
joju <whwndud92@gmail.com>
2018-06-09 19:53:18 +0900

    selenium scroll fail

--- routes/index.js
+++ routes/index.js
@@ -2,64 +2,63 @@ var express = require('express');
 var router = express.Router();
 var request = require("request");
 var cheerio = require("cheerio");
-var url_input="https://twitter.com/moonriver365";
+var async = require('async');
+var url_input = "https://twitter.com/moonriver365";
 const webdriver = require('selenium-webdriver');
+until = webdriver.until;
 const chrome = require('selenium-webdriver/chrome');
-const driver = new webdriver.Builder().forBrowser('chrome').build();
+options = new chrome.Options();
+options.addArguments('headless'); // note: without dashes
+options.addArguments('disable-gpu');
+const driver = new webdriver.Builder().forBrowser('chrome')
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .setChromeOptions(options)                         // note this
+    .build();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    //var inputUrl = "https://twitter.com/moonriver365";
-    // var inputUrl =url_input;
-    // options = {
-    //     url: inputUrl,
-    //     port: 80,
-    //     method: 'GET'
-    // };
-    // request.get(options, function (error, response, body) {
-    //     if (error) throw error;
-    //     url = 'http://gall.dcinside.com';
-    //     $ = cheerio.load(body);
-    //
-    //     $('.tweet .js-tweet-text-container').each(function () {
-    //         var tweet = $(this).text();
-    //         console.log(tweet)
-    //     });
-    // });
-    res.render('index', { title: 'Harmful Website Discrimination System' });
-});
-router.get('/',function(req,res){
-    res.render('index',{title: 'Harmful Website Discrimination System',topicHead:'URL Form',});
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Harmful Website Discrimination System', topicHead: 'URL Form',});
     console.log('user accessing Home Page');
 });
-router.post('/',function(req,res){
-    //req.check('url','url is required').notEmpty();
-    //var errors=req.validationErrors();
-    // if(errors){
-    //     errors.msg="Input required!";
-    //     res.render('index',{title: 'Harmful Website Discrimination System',topicHead:'URL Form',errors:errors});
-    //     console.log('Error');
-    // }
-    // else {
-    url_input=req.body.title;
+router.post('/', function (req, res) {
+    url_input = req.body.title;
     console.log(url_input);
-    options = {
-        url: url_input,
-        port: 80,
-        method: 'GET'
-    };
-    var tweet="";
-    request.get(options, function (error, response, body) {
-        if (error) throw error;
-        url = 'http://gall.dcinside.com';
-        $ = cheerio.load(body);
 
-        $('.tweet .js-tweet-text-container').each(function () {
-            tweet = $(this).text();
-            console.log(tweet)
-        });
+    driver.get(url_input);
+    const By = webdriver.By;
+    var curScroll = driver.executeScript("return document.body.scrollHeight");
+    var tweet = "";
+
+    var el = driver.findElements(By.css('.tweet .js-tweet-text-container')); //엘리먼트를 얻고
+    el.then(function (elements) {
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].getText().then(function (text) {
+                console.log(text);
+            }).then(function (data) {
+                // console.log(data);
+                // if(data == elements.length)
+                    // driver.close();
+            })
+        }
+    }).catch(function (err) {
+        console.error(err); // Error 출력
     });
-    res.render('index', { userValue:tweet,title: 'Harmful Website Discrimination System',topicHead:'URL Form' });
-    // }
-})
+    res.render('index', {userValue: tweet, title: 'Harmful Website Discrimination System', topicHead: 'URL Form'});
+});
+
+function waitToScrollDown(curScroll) {
+    console.log("scroll waiting...  " + curScroll);
+    driver.wait(function () {
+        console.log("scroll waiting...");
+        var script = "return document.body.scrollHeight";
+        driver.executeScript(script).then(function (data) {
+            console.log(data);
+            console.log(curScroll);
+
+            return data != curScroll
+        });
+        // console.log(changedScroll);
+    }, 3000).then(function () {
+        console.log("sefasfasdf3######################")
+    });
+}
 module.exports = router;
